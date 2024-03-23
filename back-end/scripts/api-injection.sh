58416720a68db6aeccfe30a274c845f71fefad15
Billy Euceda <billymm314@gmail.com>
2024-03-23 13:17:50 -0700

    workflow2

---
+++ back-end/scripts/api-injection.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ../infra
+# Assign the new URL
+new_url=$(terraform output -raw api_url)
+# new_url=$(terraform output -raw api_url)
+
+# Path to your JavaScript file
+file="../../front-end/javascript/visitor-counter.js"
+
+# Replace the value of apiUrl in the JavaScript file
+sed -i "s#var apiUrl = \".*\"#var apiUrl = \"$new_url\"#" "$file"
+
+# echo "apiUrl in $javascript_file has been updated to: $new_url"
+echo "$new_url"
