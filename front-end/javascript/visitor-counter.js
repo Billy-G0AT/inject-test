7926cd56ae9b7aaf5178aed3dc1ab6b90ff2910f
Billy Euceda <billymm314@gmail.com>
2024-03-27 00:16:46 -0700

    java debug

--- front-end/javascript/visitor-counter.js
+++ front-end/javascript/visitor-counter.js
@@ -1,1 +1,13 @@
 var apiUrl = "https://s086nxz7i2.execute-api.us-west-1.amazonaws.com/dev14/inject-api";
+
+var newUrl = process.argv.slice(2);
+
+if (apiUrl == newUrl) {
+    console.log("New API URL has been successfully injected.")
+    process.exit(0)
+} else {
+    console.error("URL's are not the same")
+    console.error(`Old URL is ${apiUrl}`)
+    console.error(`New URL is ${newUrl}`)
+    process.exit(1)
+}
