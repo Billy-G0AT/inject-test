319db940298cc95cabe80292455cd4b245109960
Billy Euceda <billymm314@gmail.com>
2024-03-27 01:12:25 -0700

    dev17

--- back-end/infra/api.tf
+++ back-end/infra/api.tf
@@ -8,7 +8,7 @@ resource "aws_apigatewayv2_api" "this" {
 }
 
 resource "aws_apigatewayv2_stage" "this" {
-  name        = "dev16"
+  name        = "dev17"
   api_id      = aws_apigatewayv2_api.this.id
   auto_deploy = true
 }
