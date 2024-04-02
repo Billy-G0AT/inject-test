44cd0773d4bfab8fbe135d9c9ff7e1457edc1637
Billy Euceda <billymm314@gmail.com>
2024-04-02 00:17:20 -0700

    cleanup

--- back-end/infra/api.tf
+++ back-end/infra/api.tf
@@ -8,7 +8,7 @@ resource "aws_apigatewayv2_api" "this" {
 }
 
 resource "aws_apigatewayv2_stage" "this" {
-  name        = "finaltest"
+  name        = "cleanup"
   api_id      = aws_apigatewayv2_api.this.id
   auto_deploy = true
 }
--- front-end/javascript/visitor-counter.js
+++ front-end/javascript/visitor-counter.js
@@ -1,13 +1,1 @@
 var apiUrl = "https://ijk0595309.execute-api.us-west-1.amazonaws.com/finaltest/inject-api";
-
-var newUrl = process.argv.slice(2);
-
-if (apiUrl == newUrl) {
-    console.log("New API URL has been successfully injected.")
-    process.exit(0)
-} else {
-    console.error("URL's are not the same")
-    console.error(`Old URL is ${apiUrl}`)
-    console.error(`New URL is ${newUrl}`)
-    process.exit(1)
-}
