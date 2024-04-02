e8dc5d070a8abcbf06dc16f4dbc6880821d99fdf
Billy Euceda <billymm314@gmail.com>
2024-04-01 23:48:04 -0700

    test

--- back-end/infra/api.tf
+++ back-end/infra/api.tf
@@ -8,7 +8,7 @@ resource "aws_apigatewayv2_api" "this" {
 }
 
 resource "aws_apigatewayv2_stage" "this" {
-  name        = "bashagain"
+  name        = "test"
   api_id      = aws_apigatewayv2_api.this.id
   auto_deploy = true
 }
