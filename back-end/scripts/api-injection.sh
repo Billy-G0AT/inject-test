b61435e459ccf78bcb0626f334e7a5f22d1fa8da
Billy Euceda <billymm314@gmail.com>
2024-04-02 00:09:39 -0700

    fix

--- back-end/scripts/api-injection.sh
+++ back-end/scripts/api-injection.sh
@@ -7,9 +7,9 @@ new_url="$1"
 file="../../front-end/javascript/visitor-counter.js"
 
 # Replace Old URL in JavaScript File
-sed -i "s#var apiUrl = \".*\"#var apiUrl = \"$new_url\"#" "$file"
+ sed -i "s#var apiUrl = \".*\"#var apiUrl = \"$new_url\"#" "$file"
 
-# Grabs URL From JS File
+# Grabs URL From JavaScript File
 apiUrl=$(grep -oP 'apiUrl\s*=\s*"\K[^"]+' $file)
 
 # Compares URL's
@@ -18,7 +18,7 @@ if [ "$apiUrl" == "$new_url" ]; then
     exit 0
 else
     echo "URL's are not the same" >&2
-    echo "Old URL is $(apiUrl)" >&2
-    echo "New URL is $(new_url)" >&2
+    echo "Old URL is $apiUrl" >&2
+    echo "New URL is $new_url" >&2
     exit 1
 fi
